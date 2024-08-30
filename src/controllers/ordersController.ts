b35bcbeb203bbcb85dc5eca36cee860686960ad2
Fernando Rojas <fernandorojasmosqueira@gmail.com>
2024-08-30 05:12:40 +0000

    fix: populate clients

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -22,21 +22,23 @@ export const createOrder = async (req: Request, res: Response) => {
     console.log(error);
     res.status(500).json({ data: {}, message: "Error interno del servidor", error: true });
   }
-};
+}
 
 export const getOrders = async (req: Request, res: Response) => {
   try {
-    
-    const page = req?.query?.page ? Number(req.query.page) : 1;
-    const limit = req?.query?.limit ? Number(req.query.limit) : 10;
+    const page = req.query.page ? Number(req.query.page) : 1;
+    const limit = req.query.limit ? Number(req.query.limit) : 10;
+    const { user_id } = req.body;
 
-    const { user_id } = req.body; 
-
-    const skip = (page - 1) * limit; // Calcula el número de documentos a saltar
+    const skip = (page - 1) * limit;
 
     const orders = await Order.find({ user_id, deleted_at: null })
                               .skip(skip)
-                              .limit(limit);
+                              .limit(limit)
+                              .populate({
+                                path: 'client_id',
+                                select: 'name'
+                              });
 
     const totalOrders = await Order.countDocuments({ user_id, deleted_at: null });
 
@@ -64,4 +66,3 @@ export const getOrders = async (req: Request, res: Response) => {
     res.status(500).json({ data: {}, message: "Error interno del servidor", error: true });
   }
 };
-
